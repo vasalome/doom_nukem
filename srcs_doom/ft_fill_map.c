155aa2d68f7da78a4a3597960ecc5f8f7288aded
Valentin Salome <vasalome@z1r12p6.le-101.fr>
2019-10-10 17:48:08 +0200

    suite_propal_map

--- srcs_doom/ft_fill_map.c
+++ srcs_doom/ft_fill_map.c
@@ -6,7 +6,7 @@
 /*   By: vasalome <vasalome@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/11 15:40:07 by vasalome     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/08 17:28:52 by vasalome    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/10 17:47:36 by vasalome    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -72,3 +72,31 @@ int		fill_map(t_info *info)
 	info->map.map[fill.x][fill.y] = '\0';
 	return (0);
 }
+
+/*
+int		parser(t_info *info)
+{
+	...
+gnl
+
+	case_type(fill[i][0]);
+	if (fill[i][1] == '1')
+		colision(fill[i][1]);
+	if (fill[i][2] == '1')
+		ray_passe ; ?
+	height_floor(fill[i][3]);
+	if (fill[i][4] == '1')
+		roof(fill[i][4], fill[i][5]);
+	textures_wall(fill[i][6],fill[i][7],fill[i][8],fill[i][9]);
+	if (fill[i][10] == '1')
+		teleport(fill[i][11]);
+	if (fill[i][12] != '0')
+		music(fill[i][12]);
+	
+}
+
+	// idee formatage parsing
+	// [1,0,1,1.25,1,2.25,N,E,S,O,1,10 11 A,1]
+	// [fill[i][0],fill[i][1],fill[i][2],fill[i][3],fill[i][4],fill[i][5],.....]
+
+*/
