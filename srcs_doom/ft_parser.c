e3a7b8bf9689b5a171bf6e1c034bd48eeaf4056a
Valentin Salome <vasalome@z1r12p2.le-101.fr>
2019-10-12 19:51:50 +0200

    idea_map_maj

--- srcs_doom/ft_parser.c
+++ srcs_doom/ft_parser.c
@@ -6,39 +6,62 @@
 /*   By: vasalome <vasalome@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/11 15:40:07 by vasalome     #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/11 18:19:21 by vasalome    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/12 19:50:05 by vasalome    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "../include_doom/doom.h"
 
-static int	inside(char,*****)
+void	check_inside(type variable)
 {
+	int		i;
+	int		count;
+	i = -1;
+	count = 0;
+
+	while (variable[++i])
+		if (variable[i] == ',')
+			count++;
+	if (count != 13)
+		ft_usage('Invalid case:'variable);
+}
+
+static int	inside(type variable, int i)
+{
+	int	
+	check_inside();
+	while (variable[*i] != ',')
+		if (!variable[*i] >= '1' && variable[*i] <= 'ID MAX DES MURS')
+			ft_usage('Invalid case:'variable);
+	{
+		
+	}
 	
 }
 
 
 
-static int	parser(?? info ??)
+static int	parser(?? variable ??)
 {
 	int		i;
 	int		top;
 	int		buff[BUFF_SIZE];
 
-	i = 0;
+	i = -1;
 	top = 0;
-	while (info[++i])
+	while (variable[++i])
 	{
-		if (info[i] == '[')
-			buff[++top] = info[i];
-		if (info[i] == ']')
-			if (!inside(buff[top--], info[i]))
-				return (0);
+		if (variable[i] == '[')
+			buff[++top] = variable[i];
+		if (variable[i] == ']')
+			if (!inside(buff[top--], variable[i]))
+				return (-1);
 	}
 
 
 -----------------
+
 
 	case_type(fill[i][0]);
 	if (fill[i][1] == '1')
