d224cb9e545d91409f1e8bfea2e8a164685e68b1
Valentin Salome <vasalome@z1r13p3.le-101.fr>
2019-11-22 13:36:45 +0100

    approche_circle

--- srcs_doom/ft_circle_wall.c
+++ srcs_doom/ft_circle_wall.c
@@ -2,23 +2,29 @@
 
 //mdr
 
-ray_circle({x: info->player.x_pos, y: info->player.y_pos}, {x: info->player.x_pos + info->ray.x_ray_direction, y: info->player.y_pos + info->ray.y_ray_direction}, {x: mapX + 0.5, y: mapY + 0.5}, 0.5);
+ray_circle(info, 0.5);
 
-int     ray_circle(p1, p2, sc, r)
+int     ray_circle(t_info *info, double r)
 {
     double a, b, c;
     double bb4ac;
-    double dp{0, 0};
+    double *dp = {0, 0};
+    double intersectDist1;
+    double intersectDist2;
 
-    dp[0] = p2.x - p1.x;
-    dp[1] = p2.y - p1.y;
+    dp[0] = (info->player.x_pos + info->ray.x_ray_direction) - info->player.x_pos;
+    dp[1] = (info->player.y_pos + info->ray.y_ray_direction) - info->player.y_pos;
     a = dp[0] * dp[0] + dp[1] * dp[1];
-    b = 2 * (dp[0] * (p1.x - sc.x) + dp[1] * (p1.y - sc.y));
-    c = sc.x * sc.x + sc.y * sc.y;
-    c += p1.x * p1.x + p1.y * p1.y;
-    c -= 2 * (sc.x * p1.x + sc.y * p1.y);
+    b = 2 * (dp[0] * (info->player.x_pos - (floor(info->player.x_pos) + 0.5)) + dp[1] * (info->player.y_pos - (floor(info->player.y_pos) + 0.5)));
+    c = (floor(info->player.x_pos) + 0.5) * (floor(info->player.x_pos) + 0.5) + (floor(info->player.y_pos) + 0.5) * (floor(info->player.y_pos) + 0.5);
+    c += info->player.x_pos * info->player.x_pos + info->player.y_pos * info->player.y_pos;
+    c -= 2 * ((floor(info->player.x_pos) + 0.5) * info->player.x_pos + (floor(info->player.y_pos) + 0.5) * info->player.y_pos);
     c -= r * r;
     bb4ac = b * b - 4 * a * c;
     if (bb4ac < 0)
         return(0);
+    intersectDist1 = (-b + sqrt(bb4ac)) / (2 * a);
+    intersectDist2 = (-b - sqrt(bb4ac)) / (2 * a);
+
+   return(intersectDist1, intersectDist2);
 }
